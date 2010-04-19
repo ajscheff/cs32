46be37981e7a590bc06892d857a8961c45b26753
andyscheff <andyscheff@9e382ece-2198-aa58-7dcd-87ca1b6e9dd7>
2010-04-19 20:15:22 +0000

    sdfasd

---
+++ system/application/config/email.php
@@ -0,0 +1,8 @@
+<?php
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'smtp.sendgrid.net';
+$config['smtp_user'] = 'andyscheff@gmail.com';
+$config['smtp_pass'] = 'mobi10';
+$config['smtp_port'] = '25';
+$config['mailtype'] = 'html';
+$config['mailpath'] = '/usr/sbin/sendmail';
