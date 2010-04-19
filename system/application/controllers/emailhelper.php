3daa3f4d7288550c26e5a2bb457d3995b5798af2
andyscheff <andyscheff@9e382ece-2198-aa58-7dcd-87ca1b6e9dd7>
2010-04-19 20:40:00 +0000

    madfsad

---
+++ system/application/controllers/emailhelper.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+*	
+*/
+class EmailHelper extends Controller {
+
+	function EmailHelper() {
+		parent::Controller();		
+	}
+	
+	function send(){
+		$this->load->library('email');
+
+		$this->email->from('test@ombtp.com', 'Test');
+		$this->email->to('andyscheff@gmail.com'); 
+		
+		$this->email->subject('Email Test');
+		$this->email->message('Testing the email class.');	
+		
+		$this->email->send();
+		
+		echo $this->email->print_debugger();
+	}
+	
+	function receive(){
+	
+		$this->load->database();
+		
+		$this->db->set('to', $_POST['to']);
+		$this->db->set('from', $_POST['from']);
+		$this->db->set('message', $_POST['text']);
+		$this->db->insert('email_test');
+		
+	}
+}
