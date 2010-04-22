294c69484582851ffb8d76fb51a9f57b98858116
andyscheff <andyscheff@9e382ece-2198-aa58-7dcd-87ca1b6e9dd7>
2010-04-22 20:31:35 +0000

    working still..

--- trunk/system/application/models/circles.php
+++ trunk/system/application/models/circles.php
@@ -37,9 +37,9 @@ class Circles extends Model {
 
 	function getMemberEmails($circle_id) {
 		$this->db->select('users.phone_number, providers.gateway');
-		$this->db->from('users');
+		$this->db->from('users, providers');
 		$this->db->join('users_circles', 'users_circles.user_id = users.id');
-		//$this->db->join('providers', 'users.provider_id = providers.id');
+		$this->db->where('users.provider_id', 'providers.id');
 		$this->db->where('users_circles.circle_id', $circle_id);
 
 		$query = $this->db->get();
