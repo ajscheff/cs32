df324536f32f957338ceb616b55d9d5d5b35d4c8
lancejabr <lancejabr@9e382ece-2198-aa58-7dcd-87ca1b6e9dd7>
2010-04-22 02:24:30 +0000

    new views APR 21

--- trunk/system/application/views/home.php
+++ trunk/system/application/views/home.php
@@ -1,5 +1,34 @@
 <html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="/css/base.css" />
+	</head>
 	<body>
-		<h1>Hello, <?php echo $username; ?> password: <?php echo $password ?>!</h1>
+			<div id="backshade">
+			<div id="panel">
+					<div id="masthead">
+						<a id="title" href="http://www.google.com">mobi</a>
+						<div id="logo"></div>
+						<p id="subtitle">mobile social networking</p>
+					</div>
+					<div class="divider" style="height: 3px; width: 100%;"/>
+					<table id="interactions">
+					<tr>
+						<td id="circlelist">
+						<h4 style="margin-bottom: 10px;">Your circles:</h4>
+						<p id="selectedcircle" class="circle">mobi</p>
+						<p class="circle">mobi2</p>
+						<p class="circle">mobi3</p>
+						<p class="circle">mobi4</p>
+						
+						</td>
+						<td class="divider" style="width:3px; height=100%;"></td>
+						<td>
+
+						</td>
+					</tr>
+					</table>
+			</div>
+			</div>
+			<div id="bottomshade"></div>
 	</body>
 </html>
--- trunk/system/application/views/login.php
+++ trunk/system/application/views/login.php
@@ -3,41 +3,47 @@
 		<link rel="stylesheet" type="text/css" href="/css/base.css" />
 	</head>
 	<body>
-		<div id="root">
 			<div id="backshade">
 			<div id="panel">
-					<table id="masthead">
-					<tr>
-						<td><h1 id="logo">mobi</h1></td>
-						<td style="width: 6px;"/>
-						<td><h5 id="sublogo">mobile social networking</h5></td>
-					<tr>
-					</table>
-					<div class="divider" style="height: 3px; width: 100%;"/>
-					<div class="divider" style="height: 400px; width: 800px; margin: auto;">
-						<h1>please signup:</h1>
-						<form method="post" action="/index.php/welcome/createUser">
-						username:
-						<input type="text" name="username"/>
-						password:
-						<input type="password" name="password"/>
-						<input type="submit" value="Go!"/>
-						</form>
-						<h1>please login:</h1>
-						<form method="post" action="/index.php/welcome/login">
-						username:
-						<input type="text" name="username"/>
-						password:
-						<input type="password" name="password"/>
-						<input type="submit" value="Go!"/>
-						</form>
-						<form method="post" action="/~magartne/cs32/index.php/welcome/sendEmail">
-						Test Email:
-						<input type="submit" value="Send Test"/>
-						</form>
+					<div id="masthead">
+						<a id="title" href="http://www.google.com">mobi</a>
+						<p id="subtitle">mobile social networking</p>
 					</div>
+					<div class="divider" style="height: 3px; width: 100%;"/>
+					<table id="interactions">
+					<tr>
+						<td>
+						<h2>Welcome to mobi!</h2>
+						<br/>
+						<p> Mobi is a new service that let's groups of people stay in contact on the go.  It's fast, free, and easy!</p>
+						<br/>
+						<a href="http://www.google.com">learn more</a>
+						</td>
+						<td class="divider" style="width:3px; height=100%;"></td>
+						<td>
+							<h1>please signup:</h1>
+							<form method="post" action="/index.php/welcome/createUser">
+							<p>username:</p>
+							<input type="text" name="username"/>
+							<p>password:</p>
+							<input type="password" name="password"/>
+							<input type="submit" value="Go!"/>
+							</form>
+							<h1>please login:</h1>
+							<form method="post" action="/index.php/welcome/login">
+							<p>username:</p>
+							<input type="text" name="username"/>
+							<p>password:</p>
+							<input type="password" name="password"/>
+							<input type="submit" value="Go!"/>
+							</form>
+						</td>
+					</tr>
+					</table>
+					<div id="whaleback"></div>
+
 			</div>
 			</div>
-		</div>	
+			<div id="bottomshade"></div>
 	</body>
 </html>
