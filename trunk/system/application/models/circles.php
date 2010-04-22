86262eea0344f24bc15dcad7368964145a924386
andyscheff <andyscheff@9e382ece-2198-aa58-7dcd-87ca1b6e9dd7>
2010-04-22 20:25:39 +0000

    working still..

--- trunk/system/application/models/circles.php
+++ trunk/system/application/models/circles.php
@@ -22,4 +22,34 @@ class Circles extends Model {
 		if (empty($row)) return 0;
 		else return $row['id'];
 	}
+
+	function getPermissions($user_id, $circle_id){
+		$this->db->select('privileges');
+		$this->db->from('users_circles');
+		$this->db->where('user_id', $user_id);
+		$this->db->where('circle_id', $circle_id);
+		$query = $this->db->get();
+
+		$row = $query->row_array();
+		if (empty($row)) return NULL;
+		else return $row['privileges'];
+	}
+
+	function getMemberEmails($circle_id) {
+		$this->db->select('users.phone_number, providers.gateway');
+		$this->db->from('users');
+		$this->db->where('users_circles.circle_id', $circle_id);
+		$this->db->join('users_circles', 'users_circles.user_id = users.id');
+		$this->db->join('providers', 'users.provider_id = providers.id');
+		
+		$query = $this->db->get();
+
+		$rows = $query->row_array();
+
+		print_r($rows);
+	}
+
+	function getAdminEmails($circle_id) {
+
+	}	
 }
