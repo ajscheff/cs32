6f37cc9b7e1263bcb1023639b5c4288eeff9f2dd
andyscheff <andyscheff@9e382ece-2198-aa58-7dcd-87ca1b6e9dd7>
2010-04-29 19:13:47 +0000

    dafdas

--- system/application/controllers/emailhelper.php
+++ system/application/controllers/emailhelper.php
@@ -18,26 +18,23 @@ class EmailHelper extends Controller {
 		$this->load->model('Messages');
 
 		//get the phone number that the email was sent from
-		/*$numberFrom = substr($_POST['from'], 1, 10);
+		$numberFrom = substr($_POST['from'], 1, 10);
 		$email = substr($_POST['to'], 0, strlen($_POST['to'])-11);
 
 		$this->db->set('from', $numberFrom); //for debugging, remove later
 		$this->db->set('to', $email);
 		$this->db->set('message', $_POST['text']);
-		$this->db->insert('email_test');*/
+		$this->db->insert('email_test');
 
 		//get the user and circle id's, will be 0 if they dont exist
-		$user_id = $this->Users->phoneExists('4015276563');
-		$circle_id = $this->Circles->circleExists('mobiteam');
+		$user_id = $this->Users->phoneExists($numberFrom);
+		$circle_id = $this->Circles->circleExists($email);
 
-		echo $user_id;
-		echo $circle_id;
-		echo 'dafksda';
 		
 		//if the phone number exists in our database
 		if ($user_id != 0) {
 			if($circle_id != 0) {
-				$this->Messages->validEmailReceived($user_id, $circle_id, 'mobiteam', 'message');
+				$this->Messages->validEmailReceived($user_id, $circle_id, $email, $_POST['text']);
 			}
 			else {
 				//circle doesn't exists.. send reply?
@@ -46,10 +43,6 @@ class EmailHelper extends Controller {
 		else {
 			//phone doesn't exist, send reply?
 		}
-
-		
-
-		//$this->Messages->validEmailReceived(1, 1, 'mobiteam', 'message');
 		
 	}
 }
