9388246e0e9c7c48f83b48f4109a1a715c69840c
lancejabr <lancejabr@9e382ece-2198-aa58-7dcd-87ca1b6e9dd7>
2010-05-01 21:01:09 +0000

    new views

--- trunk/system/application/views/home.php
+++ trunk/system/application/views/home.php
@@ -1,6 +1,41 @@
 <html>
 	<head>
+		<?php echo '<title>mobi: '.$username.'</title>'; ?>
 		<link rel="stylesheet" type="text/css" href="/css/base.css" />
+		<link rel="stylesheet" type="text/css" href="/css/home.css" />
+		<script type="text/javascript" src="http://code.jquery.com/jquery-1.4.min.js"></script>
+		<script type="text/javascript"><!--
+				
+				function loadCircle($username, $circlename) {
+					alert(username);
+					}
+					
+					// get the values from the form
+//					var value = $('#value').val();
+//					var checked = $('#append').attr('checked');
+//					
+					
+					// now, make an ajax call to the server to
+					// get the return value for this input.
+					// the return value will be in the variable
+					// 'data'
+//						$.post('/welcome/ajax_form_post/', { value : value }, function(data) {
+					
+						// set it in the displayHere paragraph
+//						$('#displayHere').html(data);
+						
+						// finally, show the paragraph with id 'displayHere'
+//						$('#displayHere').show();
+						
+						// append it to the div 'appendToMe' if the checkbox is checked
+						
+//						if (checked)
+//							$('#appendToMe').append(data);
+					
+//					});
+					
+					
+			--></script>
 	</head>
 	<body>
 			<div id="backshade">
@@ -9,33 +44,20 @@
 						<a id="title" href="/index.php/welcome/index">mobi</a>
 						<p id="subtitle">mobile social networking</p>
 						<p id="userstatus">Hello, <?php echo $username ?>!</p>
-						<?php echo'<a id="settingsbutton" href="/index.php/settings/loadSettings/'.$username.'">settings</a>';?>
+						<?php echo '<a id="settingsbutton" href="/index.php/settings/loadSettings/'.$username.'">settings</a>';?>
 					</div>
 					<div class="divider" style="height: 3px; width: 100%;"/>
-					<table id="interactions">
-					<tr>
-						<td id="circlelist">
-						<h4 style="margin-bottom: 10px;">Your circles:</h4>
-							<?php foreach($circles as $circle) {
-								echo '<p class="circle">'.$circle['name'].'</p>';
-								}
+					<div id="interactions">
+						<div id="circlelist">
+							<h4 style="margin-bottom: 10px;">Your circles:</h4>
+								<?php foreach($circles as $circle) {
+									echo '<a href="javascript:loadCircle('.$username.','.$circle['name'].')">'.$circle['name'].'</a><br/><br/>';
+									}
 								?>
-						</td>
-						<td class="divider" style="width:3px; height=100%;"></td>
-						<td>
-							<h4>members:</h4>	
-							<?php foreach($circles[0]['members'] as $member) {
-								echo '<p class="member">'.$member.'</p>';
-								}
-							?>
-							<h4>recent message:<h4>
-							<?php foreach($circles[0]['messages'] as $message) {
-								echo '<p class="message">'.$message.'</p>';
-								}
-							?>
-							</td>
-					</tr>
-					</table>
+						</div>
+						<div id="circleinfo">
+						</div>
+					</div>
 					<div id="whaleback"></div>
 			</div>
 			</div>
--- trunk/system/application/views/login.php
+++ trunk/system/application/views/login.php
@@ -1,6 +1,8 @@
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="/css/base.css" />
+		<link rel="stylesheet" type="text/css" href="/css/login.css" />
+		<title>mobi</title>
 	</head>
 	<body>
 			<div id="backshade">
@@ -10,40 +12,35 @@
 						<p id="subtitle">mobile social networking</p>
 					</div>
 					<div class="divider" style="height: 3px; width: 100%;"/>
-					<table id="interactions">
-					<tr>
-						<td>
-						<h2>Welcome to mobi!</h2>
-						<br/>
-						<p> Mobi is a new service that let's groups of people stay in contact on the go.  It's fast, free, and easy!</p>
-						<br/>
-						<a href="http://www.google.com">learn more</a>
-						</td>
-						<td class="divider" style="width:3px; height=100%;"></td>
-						<td>
-							<h1>please signup:</h1>
-							<form method="post" action="/index.php/welcome/createUser">
-							<p>username:</p>
-							<input type="text" name="username"/>
-							<p>password:</p>
-							<input type="password" name="password"/>
-							<input type="submit" value="Go!"/>
-							</form>
-							<h1>please login:</h1>
-							<form method="post" action="/index.php/welcome/login">
-							<p>username:</p>
-							<input type="text" name="username"/>
-							<p>password:</p>
-							<input type="password" name="password"/>
-							<input type="submit" value="Go!"/>
-							</form>
-						</td>
-					</tr>
-					</table>
+					<div id="interactions">
+						<h2 id="welcome">Welcome to mobi!</h2>
+						<p id="summary"> Mobi is a free new service that keeps people in touch on the go.  Learn more or sign up today!</p>
+						<a id="learn" href="http://www.google.com">Learn More</a>
+						<a id="sign" href="http://www.google.com">Sign Up</a>
+						<form id="login" method="post" action="/index.php/welcome/login">
+							<h4>Sign in:</h4>
+							<input type="text" name="username" value="username"/> 
+							<br/>
+							<input type="password" name="password" value=""/>
+							<br/>
+							<input class="button" type="submit" value="Login" style="float:right"/>
+						</form>
+					</div>
+					<div id="misc">
+						(c) 2010 mobi
+						<a href="http://www.google.com">About Us</a>
+						<a href="http://www.google.com">Jobs</a>
+						<a href="http://www.google.com">Contact</a>
+						<a href="http://www.google.com">Privacy</a>
+						<a href="http://www.google.com">Terms</a>
+					</div>
 					<div id="whaleback"></div>
 
 			</div>
 			</div>
 			<div id="bottomshade"></div>
+			<div id="learnmore">
+				<p> Mobi is a free service that let's people text multiple other people at once with just one text message!</p>
+			</div>
 	</body>
 </html>
