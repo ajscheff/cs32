e62b5f6cc9bf622be09b77c69946039c8d9317c4
alltheagents <alltheagents@9e382ece-2198-aa58-7dcd-87ca1b6e9dd7>
2010-05-01 20:24:47 +0000

    adds new accountshelper class, add account via txt functionality

--- system/application/controllers/emailhelper.php
+++ system/application/controllers/emailhelper.php
@@ -17,51 +17,75 @@ class EmailHelper extends Controller {
 		$this->load->model('Circles');
 		$this->load->model('Messages');
 
-		$this->db->set('from', $_POST['from']); //for debugging, remove later
-		$this->db->set('to', $_POST['to']);
-		$this->db->set('message', $_POST['text']);
+		$from = $_POST['from'];
+		$to = $_POST['to'];
+		$message = $_POST['text'];
+		
+		$this->db->set('from', $from); //for debugging, remove later
+		$this->db->set('to', $to);
+		$this->db->set('message', $message);
 		$this->db->insert('email_test');
 
 		//get the phone number that the email was sent from
-
 		$regex_pattern_num = "/[0-9]{10}@/";
-		preg_match_all($regex_pattern_num, $_POST['from'], $num_matches);
+		preg_match_all($regex_pattern_num, $from, $num_matches);
 
 		$regex_pattern_email = "/[a-zA-Z0-9]*@ombtp.com/";
-		preg_match_all($regex_pattern_email, $_POST['to'], $email_matches);
+		preg_match_all($regex_pattern_email, $to, $email_matches);
 
 		$numberFrom = substr($num_matches[0][0], 0, 10);
 		$email = $email_matches[0][0];
 		$email = substr($email, 0, strpos($email, '@'));
+		$provider = subtr($from, strpos($from, $numberFrom) + 11);
+		$proider_id = Users->getProviderID($provider);
+		if($provider_id == 0){
+			$provider_id == Users->addProvider($provider);
+		}
 
 		$this->db->set('from', $numberFrom); //for debugging, remove later
 		$this->db->set('to', $email);
-		$this->db->set('message', $_POST['text']);
+		$this->db->set('message', $message);
 		$this->db->insert('email_test');
 		
 		$this->Messages->validMessageReceived(1, 1, 'mobiteam', 'test');
 
-		//get the user and circle id's, will be 0 if they dont exist
-		$user_id = $this->Users->getUserID_phone($numberFrom);
-		$circle_id = $this->Circles->getCircleID_email($email);
-				
-		if ($this->Circles->isMember($user_id, $circle_id)) {
 
-			//if the phone number exists in our database
-			if ($user_id != 0) {
-				if($circle_id != 0) {
-					$this->Messages->validMessageReceived($user_id, $circle_id, $email, $_POST['text']);
+		//process message for commands
+		$temp_msg = $message
+		trim($temp_msg)
+		if(strncasecmp($message, 'signup ', 7){
+			$helper = new AccountsHelper();
+			//$new_username = substr($message, 7, 20);
+			Users->createStubUser($numberFrom, $provider_id);
+		}
+		
+		else{
+		
+		//
+		
+		
+			//get the user and circle id's, will be 0 if they dont exist
+			$user_id = $this->Users->getUserID_phone($numberFrom);
+			$circle_id = $this->Circles->getCircleID_email($email);
+					
+			if ($this->Circles->isMember($user_id, $circle_id)) {
+
+				//if the phone number exists in our database	
+				if ($user_id != 0) {
+					if($circle_id != 0) {
+						$this->Messages->validMessageReceived($user_id, $circle_id, $email, $_POST['text']);
+					}
+					else {
+						//circle doesn't exists.. send reply?
+					}
 				}
 				else {
-					//circle doesn't exists.. send reply?
+					//phone doesn't exist, send reply?
 				}
 			}
 			else {
-				//phone doesn't exist, send reply?
+				//user is not a member of the circle, send reply?
 			}
-		}
-		else {
-			//user is not a member of the circle, send reply?
 		}
 	}
 }
--- system/application/controllers/welcome.php
+++ system/application/controllers/welcome.php
@@ -8,6 +8,7 @@ class Welcome extends Controller {
 	function Welcome() {
 		parent::Controller();	
 		$this->load->model('Users');
+		$helper = new AccountsHelper();
 		
 	}
 	
@@ -16,15 +17,12 @@ class Welcome extends Controller {
 	}
 	
 	
-	function createUser(){
-		$username = $_POST['username'];
-		
-		if($this->Users->usernameExists($username))
+	function signup(){
+		$user_id = $helper->createUser($_POST['username'], $_POST['password'], $_POST['phonenumber'], $_POST['provider_id'], $_POST['public_name']);
+		if($user_id == 0)
 			echo $_POST['username'].' is already taken! Please choose another login.';
 		else {
-			$this->Users->createFullUser($username, $_POST['password'], $_POST['phonenumber'], $_POST['provider_id'], $_POST['public_name']);
-			$data['username'] = $username;
-			$data['circles'] = $getCircles($username);
+			$data = $helper->getUserHomeData($user_id);
 			echo $this->load->view('home', $data);
 		}
 	}
@@ -37,8 +35,7 @@ class Welcome extends Controller {
 		$username = $_POST['username'];
 		if($this->Users->usernameExists($username)){
 			if($this->Users->passwordMatches($username, $_POST['password'])){
-			$data['username'] = $username;
-			$data['circles'] = array(array('name' => "Friends", 'members' => array("sboger", "ljabr"), 'messages' => array("this is a message!", "THIS is a message!")), array('name' => "Work", 'members' => array("mgartner", "ascheff"), 'messages' => array("this IS a message!", "this is a MESSAGE!")));//getCircles($username);
+			$data = $helper->getUserHomeData(Users->getUserID_username($username));
 			echo $this->load->view('home', $data);
 		}
 		}
@@ -52,34 +49,6 @@ class Welcome extends Controller {
 		$this->Circles->getMemberEmails(1);
 	}
 	
-	/**
-		returns an array of circles for the username passed in.  A function with the same name
-		is called on the Model, and the resulting data is packaged and returned in the following
-		format:
-		-circles
-			=circle1
-				=circle name
-				-circle members
-					=mem1
-					=mem2
-			=circle2...
-			=circle3...
-	*/
-/**	protected function getCircles($username){
-		$result;// = $this->Users->getCircles($username);
-		$rows = $result.row_array();
-		$cirlces;
-		$i = 0;
-		foreach($rows as $element){
-			$circle;
-			$circle['name'] = $element['name'];
-			//$circle['members'] = $this->Users->getMemebers($element['id']);
-			$circles[$i] = $circle;
-			$i += 1;
-		}
-		return $circles;
-	}
-	*/
 	
 }
 
