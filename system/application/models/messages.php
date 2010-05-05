9f8d1dc894726016354b3de66fa7f04d2fd1f2f2
m.a.gartner <m.a.gartner@9e382ece-2198-aa58-7dcd-87ca1b6e9dd7>
2010-05-05 02:33:36 +0000

    asdf

--- system/application/models/messages.php
+++ system/application/models/messages.php
@@ -74,8 +74,8 @@ class Messages extends Model {
 
 		foreach ($emailList as $contact) {
 			//if ($user_id != $contact->user_id) {
-				$username = $this->Users->getUsername($user_id);
-				$message = 'USER: ' . $username . "\n" . $message;
+				$username = $this->Users->getPublicName($user_id);
+				//$message = 'USER: ' . $username . "\n" . $message;
 				$from = ' ' . $username . ' in circle ' . $circle_email . '@ombtp.com';
 				//$from = 'wtf';
 				$this->send($from, $contact->phone_number.'@'.$contact->gateway, $message);
