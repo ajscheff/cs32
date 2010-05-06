bc6dcf4d5ae16cd9403a24bb6c862d617f51df3e
lancejabr <lancejabr@9e382ece-2198-aa58-7dcd-87ca1b6e9dd7>
2010-05-06 03:22:14 +0000

    sdf

--- system/application/views/circleinfo.php
+++ system/application/views/circleinfo.php
@@ -1,25 +1,30 @@
 			<div id="circleheader">
-				<?php echo '<h4>'.$name.'</h4>';
-						echo '<p>'.$description.'</p>';
-						
-						if(is_admin>0){
-							echo '<a id="deletebutton" href="javascript:deleteCircle('.$circle_id.')">Delete Circle</a>';
-						}
-						
-						?>
+				<?php
+					if($is_admin != '0')
+						echo '<a id="deletebutton" href="javascript:deleteCircle('.$circle_id.')">Delete Circle</a>';	
+					echo '<h3>'.$name.'</h3>';
+					echo '<p>'.$description.'</p>';	
+
+				?>
 			</div>
 			<div>
 				<div id="adminslist">
-					<h5>admins:</h5>	
+					<h4 style="padding-bottom: 3px;">admins:</h4>	
 					<?php foreach($admins as $admin) {
 						echo '<p class="admin">'.$admin.'</p>';
 						}
 					?>
+					<br/>
+					<h4 style="padding-bottom: 3px;">members:</h4>
+					<?php foreach($members as $member) {
+						echo '<p class="admin">'.$member.'</p>';
+						}
+					?>
 				</div>
 				<div id="messagelist">
-					<h5>recent messages:<h5>
+					<h4 style="padding-bottom: 3px;">recent messages:<h4>
 					<?php foreach($messages as $message) {
-						echo '<p class="message">asf'.$message.'</p>';
+						echo '<p class="message">'.$message.'</p>';
 						}
 					?>
 				</div>
