6efa48dcc53caaf22608c85090f4d95a8e5e6673
andyscheff <andyscheff@9e382ece-2198-aa58-7dcd-87ca1b6e9dd7>
2010-05-07 07:47:04 +0000

    afsadfsd

--- system/application/controllers/home.php
+++ system/application/controllers/home.php
@@ -79,13 +79,11 @@ class Home extends Controller {
 	
 	function deleteCircle() {
 		$this->Circles->deleteCircle($_POST['circle_id']);
-		redirect('/home/loadHomeView/0');
 	}
 	
 	function leaveCircle() {
 		$user_id = $this->session->userdata('user_id');
 		$this->Users->removeUserFromCircle($user_id, $_POST['circle_id']);
-		redirect('/home/loadHomeView/0');
 	}
 	
 
