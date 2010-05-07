d52f8e0d5f972f3609c96fbf40036eb4b8145f6a
lancejabr <lancejabr@9e382ece-2198-aa58-7dcd-87ca1b6e9dd7>
2010-05-07 17:36:34 +0000

    asdf

--- trunk/system/application/views/circleinfo.php
+++ trunk/system/application/views/circleinfo.php
@@ -21,7 +21,10 @@
 					<br/>
 					<h4 style="padding-bottom: 3px;">members:</h4>
 					<?php foreach($members as $member) {
-						echo '<p class="admin">'.$member['public_name'].'<a class="userdeletebutton" href="javascript:deleteUserFromCircle('.$member['user_id'].','.$circle_id.')">x</a></p>';
+						echo '<p class="admin">'.$member['public_name'];
+						if($is_admin == 1) 
+							echo '<a class="userdeletebutton" href="javascript:deleteUserFromCircle('.$member['user_id'].','.$circle_id.')">x</a>';
+						echo '</p>';
 						}
 					?>
 				</div>
