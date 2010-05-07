1ea2128ef1afb86d8f7f76f1b6db4a967bc5342b
lancejabr <lancejabr@9e382ece-2198-aa58-7dcd-87ca1b6e9dd7>
2010-05-07 18:10:34 +0000

    asdf

--- system/application/controllers/home.php
+++ system/application/controllers/home.php
@@ -59,13 +59,14 @@ class Home extends Controller {
 	
 	function addUser() {
 		$user_id = $this->Users->getUserID_phone($_POST['phone_number']);
+		$circle_id = $_POST['circle_id'];
 				
 		if ($user_id == 0) {
 			$user_id = $this->Users->createStubUser($_POST['phone_number']);
 		}
 		
-		$this->Users->addUserToCircle($user_id, $_POST['circle_id'], $_POST['public_name']);
-		$this->loadCircle_id($_POST['circle_id']);
+		$this->Users->addUserToCircle($user_id, $circle_id, $_POST['public_name']);
+		$this->loadCircle_id($circle_id);
 	}
 	
 	function createCircle() {
